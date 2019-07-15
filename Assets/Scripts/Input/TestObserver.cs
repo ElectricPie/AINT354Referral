6360be1a7c8559730443df3efcc77b01494b1244
Connor Priest <Kon_Priest@Hotmail.com>
2019-07-15 16:44:47 +0100

    Created Input Handler
    Created an input handler using the observer pattern so that any object will be able to register an observer with an axis, button or keycode and will be notified when that input is activated.

---
+++ Assets/Scripts/Input/TestObserver.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObserver : InputObserver
+{
+    public override void OnInputEvent(float value)
+    {
+        Debug.Log("Triggered");
+    }
+}
+
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private static InputHandler m_inputHandler;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_inputHandler = this.GetComponent<InputHandler>();
+
+        m_inputHandler.AddKeyCodeObserver(new TestObserver(), KeyCode.W);
+        m_inputHandler.AddButtonObserver(new TestObserver(), "Fire1");
+        m_inputHandler.AddAxisObserver(new TestObserver(), "Horizontal", 2.0f, 2.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
