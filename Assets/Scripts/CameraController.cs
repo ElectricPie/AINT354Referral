08cbb4227b2cf4ae1036b5fb0b6d06ca11d50248
Connor Priest <Kon_Priest@Hotmail.com>
2019-07-15 18:00:34 +0100

    Added Horizontal Camera Movement
    Added a new input observer which moves the camera to the left or right based on the "horizontal" axis when notified.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //Private
+    private InputHandler m_inputHandler;
+    private HorizontalCameraAxisObserver m_horizontalAxisObserver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_inputHandler = this.GetComponent<InputHandler>();
+
+        m_horizontalAxisObserver = new HorizontalCameraAxisObserver(this.gameObject);
+
+        m_inputHandler.AddAxisObserver(m_horizontalAxisObserver, "Horizontal", 0.1f, -0.1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Input/InputObservers/HorizontalCameraAxisObserver.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalCameraAxisObserver : InputObserver
+{
+    private GameObject m_camera;
+
+    public HorizontalCameraAxisObserver()
+    {
+        m_camera = null;
+    }
+
+    public HorizontalCameraAxisObserver(GameObject camera)
+    {
+        m_camera = camera;
+    }
+
+    public override void OnInputEvent(float value)
+    {
+        m_camera.transform.position += m_camera.transform.right * value * 0.1f;
+    }
+}
