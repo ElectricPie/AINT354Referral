5faf09dcb3f2f08b81f969645e060a12b6ff7875
Connor Priest <Kon_Priest@Hotmail.com>
2019-07-17 17:09:19 +0100

    Create Generic Attackable Object and Unit
    Create a attackable class which will be used by both units and buildings to store their health, what happens when they are attacked, name and a description about it.
    Create a sub class of attackable which will be used to store a unit’s damage information and other unit specific information.
    Create a test unit sub class of unit that just test that the unit correctly executes its die function when it has received damage to reduce its health below 0.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Unit testUnit;
+
     private static InputHandler m_inputHandler;
 
     // Start is called before the first frame update
     void Start()
     {
         m_inputHandler = this.GetComponent<InputHandler>();
+
+        InvokeRepeating("TestDamage", 0, 1.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void TestDamage()
+    {
+        if (testUnit != null)
+        testUnit.ReciveAttack(1);
     }
 }
