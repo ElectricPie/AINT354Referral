f3fd0b803a0aa6f01a65946c6333e982acede7c4
Connor Priest <Kon_Priest@Hotmail.com>
2019-07-17 15:04:08 +0100

    Added Vertical Camera Movement
    Added a new observer for handling vertical movement.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,15 +9,14 @@ public class CameraController : MonoBehaviour
     //Private
     private InputHandler m_inputHandler;
     private HorizontalCameraAxisObserver m_horizontalAxisObserver;
+    private VerticalCameraAxisObserver m_verticalAxisObserver;
 
     // Start is called before the first frame update
     void Start()
     {
         m_inputHandler = this.GetComponent<InputHandler>();
 
-        m_horizontalAxisObserver = new HorizontalCameraAxisObserver(this);
-
-        m_inputHandler.AddAxisObserver(m_horizontalAxisObserver, "Horizontal", 0.1f, -0.1f);
+        SetUpObservers();
     }
 
     // Update is called once per frame
@@ -26,8 +25,24 @@ public class CameraController : MonoBehaviour
         
     }
 
+    private void SetUpObservers()
+    {
+        //Creates the observers
+        m_horizontalAxisObserver = new HorizontalCameraAxisObserver(this);
+        m_verticalAxisObserver = new VerticalCameraAxisObserver(this);
+
+        //Sets the observers to listen for their input
+        m_inputHandler.AddAxisObserver(m_horizontalAxisObserver, "Horizontal", 0.1f, -0.1f);
+        m_inputHandler.AddAxisObserver(m_verticalAxisObserver, "Vertical", 0.1f, -0.1f);
+    }
+
     public void MoveCameraHorizontally(float directionValaue)
     {
         this.transform.position += this.transform.right * directionValaue * speed;
+    }
+
+    public void MoveCameraVertically(float directionValaue)
+    {
+        this.transform.position += this.transform.forward * directionValaue * speed;
     }
 }
---
+++ Assets/Scripts/Input/InputObservers/VerticalCameraAxisObserver.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalCameraAxisObserver : AxisCameraObserver
+{
+    public VerticalCameraAxisObserver() : base() { }
+    public VerticalCameraAxisObserver(CameraController cameraController) : base(cameraController) { }
+
+    public override void OnInputEvent(float value)
+    {
+        m_cameraController.MoveCameraVertically(value);
+    }
+}
