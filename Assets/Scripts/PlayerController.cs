1b0f6aaeb4d1adbac965caed45f88ccc6c792a14
Connor Priest <Kon_Priest@Hotmail.com>
2019-07-17 16:09:00 +0100

    Removed Test Observers
    Removed the test observers as they are no longer useful.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,14 +10,6 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         m_inputHandler = this.GetComponent<InputHandler>();
-
-        TestObserver axisTest = new TestObserver();
-
-        m_inputHandler.AddKeyCodeObserver(new TestObserver(), KeyCode.W);
-        m_inputHandler.AddButtonObserver(new TestObserver(), "Fire1");
-        m_inputHandler.AddAxisObserver(axisTest, "Horizontal", 2.0f, 2.0f);
-
-        m_inputHandler.RemoveAxisObserver(axisTest);
     }
 
     // Update is called once per frame
