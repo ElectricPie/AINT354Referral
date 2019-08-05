4254e8141688bbc024289ab5a6b48c6346a8f517
Connor Priest <Kon_Priest@Hotmail.com>
2019-08-05 16:49:32 +0100

    Added Grid Snapping
    Buildings now snap to a grid based of a grid spacing variable stored withing a grid object in the scene.
    The grid can also displays a gizmo to make it easy to see the spacing.

--- Assets/Scripts/Buildings/BuildingGhost.cs
+++ Assets/Scripts/Buildings/BuildingGhost.cs
@@ -8,12 +8,17 @@ public class BuildingGhost : MonoBehaviour
     //Public
     public GameObject ghost;
 
+    //Private
+    private BuildingGrid m_grid;
+
     // Start is called before the first frame update
     void Start()
     {
         this.gameObject.layer = 2;
         //this.GetComponent<Collider>().enabled = false;
         this.GetComponent<NavMeshObstacle>().enabled = false;
+
+        m_grid = GameObject.Find("_Grid").GetComponent<BuildingGrid>();
     }
 
     // Update is called once per frame
@@ -24,7 +29,15 @@ public class BuildingGhost : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit))
         {
-            this.transform.position = hit.point;
+            //this.transform.position = hit.point;
+            if (m_grid != null)
+            {
+                this.transform.position = m_grid.GetNearestPoint(hit.point);
+            }
+            else
+            {
+                Debug.LogWarning("No grid found on: " + this);
+            }
         }
     }
 }
---
+++ Assets/Scripts/Buildings/BuildingGrid.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingGrid : MonoBehaviour
+{
+    //Public
+    public int debugGridWitdh = 10;
+    public int debugGridHeight = 10;
+    public float gridSpacing = 1.0f;
+
+    public Vector3 GetNearestPoint(Vector3 position)
+    {
+        //Rounds the given position to the closest point on the grid
+        position.x = Mathf.Round(position.x * 1 / gridSpacing) * gridSpacing;
+        position.z = Mathf.Round(position.x * 1 / gridSpacing) * gridSpacing;
+
+        return position;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0, 0, 1, 1);
+
+        for (int i = 0; i < debugGridWitdh; i++)
+        {
+            for (int j = 0; j < debugGridHeight; j++)
+            {
+                Gizmos.DrawCube(new Vector3(i * gridSpacing, 1, j * gridSpacing), new Vector3(0.1f, 0.1f, 0.1f));
+            }
+        }
+    }
+}
